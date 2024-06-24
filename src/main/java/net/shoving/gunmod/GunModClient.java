dd75fa37c067298d89ebd827cf85106beef460e6
Shovving <Shoving.arts@gmail.com>
2024-06-23 16:56:11 -0800

    added

---
+++ src/main/java/net/shoving/gunmod/GunModClient.java
@@ -0,0 +1,10 @@
+package net.shoving.gunmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GunModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
